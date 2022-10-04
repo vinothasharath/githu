a61cd29cb4aede3779fbbf255c1a17d5954dcb8c
vinothasharath <vinotha707@gmail.com>
2022-10-04 18:51:06 +0530

    master work is done

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,10 @@
+package org.bank;
+
+public class AxisBank {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\vinot\\eclipse-workspace\\BankDetails\\driver\\geckodriver-v0.31.0-win64 value");
+		
+	}
+}
+	
